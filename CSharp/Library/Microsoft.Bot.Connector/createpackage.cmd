86c56187fc76319f305802b92643396189cbeeb4
Prajakta <pracko1312@gmail.com>
2016-09-30 15:11:33 +0530

    deleted csharp

--- CSharp/Library/Microsoft.Bot.Connector/createpackage.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-setlocal enabledelayedexpansion
-setlocal enableextensions
-set errorlevel=0
-erase /s *.nupkg
-msbuild /property:Configuration=release Microsoft.Bot.Connector.csproj 
-for /f %%v in ('powershell -noprofile "(Get-Command .\bin\release\Microsoft.Bot.Connector.dll).FileVersionInfo.FileVersion"') do set version=%%v
-..\..\packages\NuGet.CommandLine.3.4.3\tools\NuGet.exe pack Microsoft.Bot.Connector.nuspec -symbols -properties version=%version%
--- CSharp/Library/createallpackages.cmd
+++
@@ -1,7 +0,0 @@
-call createpackage.cmd
-pushd Microsoft.Bot.Builder.FormFlow.Json
-call createpackage.cmd
-popd
-pushd Microsoft.Bot.Builder.Calling
-call createpackage.cmd
-popd
